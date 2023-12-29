00041b9fa06142fa9cb0dce34f38b4b0bba8f4f9
colincallebert <72252025+colincallebert@users.noreply.github.com>
2023-12-29 23:07:38 +0100

    Adding searchbar to homepage

--- Boerderij/app/src/main/java/com/example/boerderij/ui/homepage/Homepage.kt
+++ Boerderij/app/src/main/java/com/example/boerderij/ui/homepage/Homepage.kt
@@ -1,34 +1,42 @@
 package com.example.boerderij.ui.homepage
 
 import Discription
-import android.util.Log
-import android.webkit.RenderProcessGoneDetail
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.example.boerderij.data.RetrofitInstance
 import com.example.boerderij.model.activity.Activity
 import com.example.boerderij.ui.components.Function
-import androidx.navigation.NavController
-import com.example.boerderij.ui.components.Destinations
 
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Homepage(activities: List<Activity>,  goDetail: (Int) -> Unit) {
+    var searchText by remember { mutableStateOf("") }
     LazyColumn(
         modifier = Modifier
             .fillMaxSize()
@@ -37,7 +45,31 @@ fun Homepage(activities: List<Activity>,  goDetail: (Int) -> Unit) {
         item {
             Discription()
         }
-        items(activities) { activity ->
+        item {
+            OutlinedTextField(
+                value = searchText,
+                onValueChange = {
+                    searchText = it
+                },
+                placeholder = { Text("Filter op activiteit") },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Search,
+                        contentDescription = null,
+                        tint = Color.Gray
+                    )
+                },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = Color.Gray,
+                    unfocusedBorderColor = Color.Gray,
+                ),
+                shape = RoundedCornerShape(24.dp)
+            )
+        }
+        items(activities.filter { it.title.contains(searchText, ignoreCase = true) }) { activity ->
             Function(
                 activity = activity,
                 goDetail = { id ->
